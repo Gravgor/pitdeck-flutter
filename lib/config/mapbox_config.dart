c30e913eb659bf72481babfdc936bede2632dcd7
Marceli Borowczak <miloszleok@gmail.com>
2025-01-27 21:40:42 +0100

    feat: secret.

--- lib/config/mapbox_config.dart
+++ lib/config/mapbox_config.dart
@@ -1,5 +1,6 @@
 class MapboxConfig {
-  static const String accessToken = 'pk.eyJ1IjoibWJvcm93Y3phazIxMTUiLCJhIjoiY20zMnk1bXA5MWF2NTJxcXNhems4b2g1OCJ9.Jw4aE3ygRcqg9wXddMVVAQ';
+  static const String accessToken =
+      'sk.eyJ1IjoiZ3JhdmdvcjExMSIsImEiOiJjbTZmaWZyZzgwNHk2MmpxcHU4ejFoOTNpIn0.WS1Aq5U_EMcQ4pPSh398Pw';
   static const String styleUrl =
       'mapbox://styles/mapbox/dark-v11'; // Dark theme style
 }
