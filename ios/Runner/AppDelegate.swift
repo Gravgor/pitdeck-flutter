8fa23a2bd9880fbfa12d6083aef7510183fa261d
Marceli Borowczak <miloszleok@gmail.com>
2025-02-15 20:29:15 +0100

    fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,37 +5,59 @@ import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    let channelName : String = "PushNotificationChannel"
+    var deviceToken : String = ""
+
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        FirebaseApp.configure()
+        
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let pushNotificationChannel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
         
         if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: { _, _ in }
-            )
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
+        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
         }
-        
-        application.registerForRemoteNotifications()
-        
-        Messaging.messaging().delegate = self
+
+        pushNotificationChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            switch call.method {
+            case "requestNotificationPermissions":
+                self?.requestNotificationPermissions(result: result)
+            case "registerForPushNotifications":
+                self?.registerForPushNotifications(application: application, result: result)
+            case "retrieveDeviceToken":
+                self?.getDeviceToken(result: result)        
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
         
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-}
 
-// MARK: - MessagingDelegate
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
+     private func requestNotificationPermissions(result: @escaping FlutterResult) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        if let error = error {
+            result(FlutterError(code: "PERMISSION_ERROR", message: "Failed to request permissions", details: error.localizedDescription))
+            return
+        }
+            result(granted)
+        }
+    }
+
+    private func registerForPushNotifications(application: UIApplication, result: @escaping FlutterResult) {
+        application.registerForRemoteNotifications()
+        result("Device Token registration initiated")
+    }
+    
+    private func getDeviceToken(result: @escaping FlutterResult) {
+        if(deviceToken.isEmpty){
+            result(FlutterError(code: "UNAVAILABLE", message: "Device token not available", details: nil))
+        } else{
+            result(deviceToken)
+        }
     }
 }
